89bb831b08aa733cb86d072063f88104b8d215a4
mateusoro <mateusoro@gmail.com>
2021-05-19 16:58:55 -0300

    Create entrypoint.sh

--- deploy-container/entrypoint.sh
+++ deploy-container/entrypoint.sh
@@ -8,6 +8,7 @@ touch /home/coder/.config/rclone/rclone.conf
 echo $RCLONE_DATA | base64 -d > /home/coder/.config/rclone/rclone.conf
 
 rclone sync rclone: /home/coder/ -vv
+npm install tulind csv-load-sync async-get-file async-get-file express path localtunnel
 
 # Now we can run code-server with the default entrypoint
 /usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8080 $START_DIR
