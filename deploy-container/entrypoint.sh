485d2c58167be8811840344f1dbcb96a0b7cf882
mateusoro <mateusoro@gmail.com>
2021-05-20 09:56:52 -0300

    Update entrypoint.sh

--- deploy-container/entrypoint.sh
+++ deploy-container/entrypoint.sh
@@ -10,7 +10,8 @@ echo $RCLONE_DATA | base64 -d > /home/coder/.config/rclone/rclone.conf
 rclone sync rclone: /home/coder/ -vv
 echo "Instalando Node Modulos..."
 cd ~/busca
-npm install tulind csv-load-sync async-get-file async-get-file express path localtunnel
+npm install tulind csv-load-sync async-get-file async-get-file express path localtunnel cron shelljs
+node backup_automatico.js&
 
 # Now we can run code-server with the default entrypoint
 /usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8080 $START_DIR
