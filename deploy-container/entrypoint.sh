5b484aa7424ea4dc4764bfb719c3931fd6de6a9c
mateusoro <mateusoro@gmail.com>
2021-06-20 22:04:42 -0300

    Update entrypoint.sh

--- deploy-container/entrypoint.sh
+++ deploy-container/entrypoint.sh
@@ -4,5 +4,6 @@ START_DIR=/home/coder/busca
 
 cd ~/busca
 #node backup_automatico.js&
+
 # Now we can run code-server with the default entrypoint
 /usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8080 $START_DIR
