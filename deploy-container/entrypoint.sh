bdbfb81bcb485d652142ceb8dac37b8ee933ab51
mateusoro <mateusoro@gmail.com>
2021-06-30 11:15:40 -0300

    Update entrypoint.sh

--- deploy-container/entrypoint.sh
+++ deploy-container/entrypoint.sh
@@ -4,7 +4,7 @@ START_DIR=/home/coder/busca
 
 cd ~/busca
 node backup_automatico.js&
-pip3 install requests_html flask imdbpy bson pymongo dnspython imdbparser guessit flask_socketio mysql-connector-python
+#pip3 install requests_html flask imdbpy bson pymongo dnspython imdbparser guessit flask_socketio mysql-connector-python
 
 # Now we can run code-server with the default entrypoint
 /usr/bin/entrypoint.sh --bind-addr 0.0.0.0:8080 $START_DIR
