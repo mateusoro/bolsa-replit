dbc8bedaa18a4dad9385237fd3b2bb4eef4e72c1
mateusoro <mateusoro@gmail.com>
2021-08-02 16:36:40 -0300

    Delete push.sh

--- push.sh
+++
@@ -1,1 +0,0 @@
-rclone sync /home/coder/ rclone:/baskup_buscar -vv --exclude="**lib/python3.7/**" --exclude="**code-server/logs/**"  --exclude="**node_modules/**" --exclude="**\.npm/**" --exclude="**\.npm/**" --exclude="**\.cache/**"
