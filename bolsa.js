6c1518856e2cb8f48492802c97ef7ae3d4f28bb7
mateusoro <mateusoro@gmail.com>
2021-08-02 15:00:53 +0000

    Added script

--- bolsa.js
+++ bolsa.js
@@ -164,7 +164,7 @@ function emitir(resultado) {
             }
         } else {
             console.log('Esperando');
-            if()
+            if(resultado)
             setTimeout(() => {
                 console.log(resultado);
                 emitir(resultado);
