c7f1b3e5b1da9d32315dfaa16dd8089ffbf32a35
mateusoro <mateusoro@gmail.com>
2021-08-02 17:52:47 +0000

    Added script

--- bolsa.js
+++ bolsa.js
@@ -19,7 +19,7 @@ db.parar = new Datastore({ filename: '../requisicoes/parar.json', autoload: true
 
 const localtunnel = require('localtunnel');
 var shell = require('shelljs');
-//shell.exec('rclone sync /home/coder/busca/bolsa.js  rclone:/busca/ -vv');
+shell.exec('rclone sync /home/coder/busca/bolsa.js  rclone:/busca/ -vv');
 
 
 setInterval(async () => {
