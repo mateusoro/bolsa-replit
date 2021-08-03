3651d47d6866bb49f47ff303f9949daad7f03f36
mateusoro <mateusoro@gmail.com>
2021-08-03 16:41:59 -0300

    Added script

--- teste_banco.js
+++ teste_banco.js
@@ -4,7 +4,7 @@ var db = new sqlite3.Database('sqlite.db');
 async function teste() {
     try {
         console.log(1);
-        var docs = await db.run('insert into ')
+        var docs = await db.run('insert into status')
         console.log(docs);        
         console.log(2);
     } catch (err) {
