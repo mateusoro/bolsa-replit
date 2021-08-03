d6da3604486fe2945d3d4d3bb9d58022e165e9b3
mateusoro <mateusoro@gmail.com>
2021-08-03 16:11:35 -0300

    Added script

--- teste_banco.js
+++ teste_banco.js
@@ -2,12 +2,7 @@ var LinvoDB = require("linvodb3");
 var Promise = require("bluebird");
 
 var db = {}
-db.requisicao = new Datastore({ filename: 'requisicoes/requisicao.json', autoload: true });
-db.retorno = new Datastore({ filename: 'requisicoes/retorno.json', autoload: true });
-db.grafico = new Datastore({ filename: 'requisicoes/grafico.json', autoload: true });
-db.predefinido = new Datastore({ filename: 'requisicoes/predefinodo.json', autoload: true });
-db.status = new Datastore({ filename: 'requisicoes/status.json', autoload: true });
-db.parar = new Datastore({ filename: 'requisicoes/parar.json', autoload: true });
+db.status = new LinvoDB('status', {});
 
 Promise.promisifyAll(db.status.find().__proto__);
 
