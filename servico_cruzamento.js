58f121c182e35336e12f933bb2637880f7beb812
mateusoro <mateusoro@gmail.com>
2021-08-03 15:16:07 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -216,6 +216,7 @@ function sinal(lista_curta, parametro_fixo, lista_longa, operador) {
         if (lista_longa) {
             novo_parametro = lista_longa[a];
         }
+        
         if(lista_curta[a-1]){
             anterior = 
         }
