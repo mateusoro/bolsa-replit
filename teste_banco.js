1b4e1f182423a1c0cf4f3241cfa654920bc61684
mateusoro <mateusoro@gmail.com>
2021-08-03 16:13:35 -0300

    Added script

--- teste_banco.js
+++ teste_banco.js
@@ -13,7 +13,7 @@ Promise.promisifyAll(db.status.find().__proto__);
 Promise.promisifyAll(db.requisicao.find().__proto__);
 Promise.promisifyAll(db.retorno.find().__proto__);
 Promise.promisifyAll(db.grafico.find().__proto__);
-Promise.promisifyAll(db.status.find().__proto__);
+Promise.promisifyAll(db.predefinido2.find().__proto__);
 Promise.promisifyAll(db.status.find().__proto__);
 Promise.promisifyAll(db.status.find().__proto__);
 
