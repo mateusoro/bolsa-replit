286d1f53c10f0293d057eb653f302d62fb897e35
mateusoro <mateusoro@gmail.com>
2021-08-03 16:31:37 -0300

    Added script

--- teste_banco.js
+++ teste_banco.js
@@ -1,14 +1,31 @@
 var LinvoDB = require("linvodb3");
 var Promise = require("bluebird");
-var sqlite3 = require('sqlite3').verbose();
-
 
 var db = {}
-db.status = new sqlite3.Database('requisicoes/requisicao.db');
-
+db.status = new LinvoDB('status', {});
 db.requisicao =  new LinvoDB('requisicao', {});
 db.retorno =  new LinvoDB('retorno', {});
 db.grafico =  new LinvoDB('grafico', {});
 db.predefinido2 =  new LinvoDB('predefinido', {});
 db.parar =  new LinvoDB('parar', {});
 
+Promise.promisifyAll(db.status.find().__proto__);
+Promise.promisifyAll(db.requisicao.find().__proto__);
+Promise.promisifyAll(db.retorno.find().__proto__);
+Promise.promisifyAll(db.grafico.find().__proto__);
+Promise.promisifyAll(db.predefinido2.find().__proto__);
+Promise.promisifyAll(db.parar.find().__proto__);
+
+async function teste() {
+    try {
+        console.log(1);
+        var docs = await db.status.find({}).limit(10).execAsync();
+        console.log(docs);
+        var docs2 = await db.status.save({t:'1'}).execAsync();
+        console.log(docs2);
+        console.log(2);
+    } catch (err) {
+       console.log(err);
+    }
+}
+teste();
