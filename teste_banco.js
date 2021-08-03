89fe8d8a3726c45c49fb35f2b36cf937806b712f
mateusoro <mateusoro@gmail.com>
2021-08-03 16:08:42 -0300

    Added script

--- teste_banco.js
+++ teste_banco.js
@@ -9,7 +9,7 @@ Promise.promisifyAll(db.status.find().__proto__);
 try {
     console.log(1);
 	var docs = await db.status.find({ }).limit(10).execAsync();
-    console.log(2);
+    console.log(docs);
 	console.log(2);
 } catch (err) {
 	// handle errors
