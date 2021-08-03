e55bb077fbad7def79f407413a4e236c65c43ad8
mateusoro <mateusoro@gmail.com>
2021-08-03 16:12:40 -0300

    Added script

--- teste_banco.js
+++ teste_banco.js
@@ -6,7 +6,7 @@ db.status = new LinvoDB('status', {});
 db.requisicao =  new LinvoDB('requisicao', {});
 db.retorno =  new LinvoDB('retorno', {});
 db.grafico =  new LinvoDB('grafico', {});
-db.predefinido =  new LinvoDB('predefinido', {});
+db.predefinido2 =  new LinvoDB('predefinido', {});
 db.parar =  new LinvoDB('parar', {});
 
 Promise.promisifyAll(db.status.find().__proto__);
