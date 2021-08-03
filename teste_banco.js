cde426f8bbefe610851f3837fff1268b0feb4d1e
mateusoro <mateusoro@gmail.com>
2021-08-03 16:41:19 -0300

    Added script

--- teste_banco.js
+++ teste_banco.js
@@ -1,8 +1,7 @@
 var sqlite3 = require('sqlite3').verbose();
 
 
-var db = {}
-db = new sqlite3.Database('sqlite.db');
+var db = new sqlite3.Database('sqlite.db');
 
 async function teste() {
     try {
