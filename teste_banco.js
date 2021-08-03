5e7230e008f2d326130686123b5042533ae383cc
mateusoro <mateusoro@gmail.com>
2021-08-03 16:41:04 -0300

    Added script

--- teste_banco.js
+++ teste_banco.js
@@ -7,7 +7,7 @@ db = new sqlite3.Database('sqlite.db');
 async function teste() {
     try {
         console.log(1);
-        var docs = await db.query('sle')
+        var docs = await db.query('select * from status')
         console.log(docs);        
         console.log(2);
     } catch (err) {
