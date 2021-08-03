02be6f466e12aaaaccea150ddac9df8c267daa28
mateusoro <mateusoro@gmail.com>
2021-08-03 16:30:32 -0300

    Added script

--- teste_banco.js
+++ teste_banco.js
@@ -1,6 +1,8 @@
 var LinvoDB = require("linvodb3");
 var Promise = require("bluebird");
 var sqlite3 = require('sqlite3').verbose();
+
+
 var db.status = new sqlite3.Database(':memory:');
 
 db.requisicao =  new LinvoDB('requisicao', {});
