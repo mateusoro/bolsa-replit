7f9d0212eabf8f2b6d97a7acbeb4b0796d39f0be
mateusoro <mateusoro@gmail.com>
2021-08-03 17:15:18 -0300

    Added script

--- teste_banco.js
+++ teste_banco.js
@@ -5,7 +5,7 @@ async function teste() {
     try {
         console.log(1);
         // var docs = await db.run('insert into status values(null, "teste")')
-        var docs = await db.run('select * from status')
+        var docs = await db.all('select * from status')
         console.log(docs);        
         console.log(2);
     } catch (err) {
