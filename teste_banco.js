b04be5a60e534ef6611eebf863505084c0ba87e6
mateusoro <mateusoro@gmail.com>
2021-08-03 16:09:47 -0300

    Added script

--- teste_banco.js
+++ teste_banco.js
@@ -2,18 +2,18 @@ var LinvoDB = require("linvodb3");
 var Promise = require("bluebird");
 
 var db = {}
-db.status = new LinvoDB('status', {},{filename:"requisicoes/status.json"});
+db.status = new LinvoDB('status', {}, { filename: "requisicoes/status.json" });
 
 Promise.promisifyAll(db.status.find().__proto__);
 
-async function teste (){
-try {
-    console.log(1);
-	var docs = await db.status.find({ }).limit(10).execAsync();
-    console.log(docs);
-	console.log(2);
-} catch (err) {
-	// handle errors
+async function teste() {
+    try {
+        console.log(1);
+        var docs = await db.status.find({}).limit(10).execAsync();
+        console.log(docs);
+        console.log(2);
+    } catch (err) {
+        // handle errors
+    }
 }
-}
-teste()
+teste();
