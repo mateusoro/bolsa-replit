c92a880e6aacc01d9568c6b0c0f1ed428d17a5b2
mateusoro <mateusoro@gmail.com>
2021-08-04 09:15:03 -0300

    Added script

--- index.js
+++ index.js
@@ -262,7 +262,7 @@ async function atualizar_acao(nome_acao) {
                 download(nome_acao);
             } else {
                 console.log("Ação já atualizada "+ nome_acao[0]);
-                await sqlite.run('select * from status')
+                await sqlite.run('up status')
                 await db.status.update({}, { status: 'Ações atualizadas' }, { upsert: true });
 
             }
