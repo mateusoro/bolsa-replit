07d939cd17714d43ef54c5a8b2ebcf933af43c78
mateusoro <mateusoro@gmail.com>
2021-08-04 11:46:45 -0300

    Added script

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ io.sockets.on('connection', (socket) => {
 
 
     });
-    socket.on('carregar_predefinidos', async (msg) => {
+    socket.on('rodar_todos', async (msg) => {
 
         // console.log('carregar_predefinidos');
         //iniciar_cruzamente(msg);
