790f210dfa5ec14d2a63c1e80f399ae405f4b1d2
mateusoro <mateusoro@gmail.com>
2021-08-04 09:09:26 -0300

    Added script

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const sqlite = require("aa-sqlite");
 
 (async () => {
   console.log(await sqlite.open('sqlite.db'))
-  var docs = await sqlite.all('select * from status')
+  //var docs = await sqlite.all('select * from status')
   console.log(docs);
 
 })()
