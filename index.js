92d9673695cc980432c2f4cab92f7d608b3dc823
mateusoro <mateusoro@gmail.com>
2021-08-04 09:49:33 -0300

    Added script

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ setInterval(async () => {
             //console.log('Emitindo', x)
             emitir([{ destino: 'resultado', mensagem: JSON.parse(docs[x].campo) }]);
             await sqlite.run("insert into grafico values (null, '"+JSON.stringify(docs[x].campo)+"', 'S')")
-            await db.grafico.insert(docs[x]);
+            
             await sqlite.run('update retorno set ativo = "N" where id='+docs[x].id);     
             
         }
