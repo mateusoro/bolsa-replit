eda2fd4a1019e2b9bf5ad6342d00cd5ac7b9114e
mateusoro <mateusoro@gmail.com>
2021-08-04 08:56:57 -0300

    Added script

--- teste_banco.js
+++ teste_banco.js
@@ -1,10 +1,13 @@
 const aaSqlite = require("aa-sqlite");
 
   (async () => {
-    const db = await open({
-      filename: 'sqlite.db',
-      driver: sqlite3.cached.Database
-    })
+    console.log(await sqlite.open('./users.db'))
+    
+    // Adds a table
+    
+    var r = await sqlite.run('CREATE TABLE users(ID integer NOT NULL PRIMARY KEY, name text, city text)')
+    if(r) console.log("Table created")
+
     var docs = await db.all('select * from status')
     console.log(docs);     
 })()
