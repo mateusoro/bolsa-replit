e2ab9e847ac9b1cdc52508e361f4fb1f2f9d1974
mateusoro <mateusoro@gmail.com>
2021-08-04 11:27:47 -0300

    Added script

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ io.sockets.on('connection', (socket) => {
         await sqlite.run('update parar set campo = "N"');       
         for (var x in docs) {
             var d = JSON.parse(docs[x].campo);
-            d.requisicao
+            
             await sqlite.run("insert into requisicao values (null, '"+JSON.stringify(msg)+"', 'S')")
            
         }
