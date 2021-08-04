5233e775331de292e22b8f90a226927992687910
mateusoro <mateusoro@gmail.com>
2021-08-04 09:09:09 -0300

    Added script

--- teste_banco.js
+++ teste_banco.js
@@ -1,8 +1,8 @@
 const sqlite = require("aa-sqlite");
 
-  (async () => {
-    console.log(await sqlite.open('sqlite.db'))   
-    var docs = await sqlite.all('select * from status')
-    console.log(docs);   
-      
+(async () => {
+  console.log(await sqlite.open('sqlite.db'))
+  var docs = await sqlite.all('select * from status')
+  console.log(docs);
+
 })()
