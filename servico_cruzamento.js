c4b2704d62b1353fd51f0bc6367c699f53ca3a0f
mateusoro <mateusoro@gmail.com>
2021-08-04 09:32:49 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -35,7 +35,7 @@ setInterval(async () => {
             //console.log(docs[x]);
             JSON.parse()
             await db.requisicao.remove({ _id: docs[x]._id });
-            await iniciar_cruzamente(JSON.parse(docs[x]));
+            await iniciar_cruzamente(JSON.parse(docs[x].campo));
 
         }
 		//console.log('Rodando Servico');
