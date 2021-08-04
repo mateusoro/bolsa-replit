f78cb4208ac4c1ac3a3add9854aa9120366adcb5
mateusoro <mateusoro@gmail.com>
2021-08-04 17:22:48 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -685,7 +685,7 @@ async function iniciar(nome_acao, solicitacao) {
 
                                                                     for (var vv8_2 of estrategias[7].vs2) {
 
-                                                                        var longa_maior8 = await escolher_estrategia_tipo(petr4, estrategias, 5, vv6_1, vv6_2, longa_maior5);
+                                                                        var longa_maior8 = await escolher_estrategia_tipo(petr4, estrategias, 7, vv6_1, vv6_2, longa_maior5);
 
                                                                         for (var vv9_1 of estrategias[8].vs1) {
 
