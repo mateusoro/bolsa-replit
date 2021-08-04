84c77f7578f6400501c7911f1bcd2d197e0857a6
mateusoro <mateusoro@gmail.com>
2021-08-04 09:43:57 -0300

    Added script

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ io.sockets.on('connection', (socket) => {
         //iniciar_cruzamente(msg);
         await sqlite.run('update parar set campo = "N"');
         var t = JSON.stringify(msg);
-        console.log(t);
+        console.log("insert into requisicao values (null, '"+JSON.stringify(msg)+"', 'S')");
         await sqlite.run("insert into requisicao values (null, '"+JSON.stringify(msg)+"', 'S')")
         
 
