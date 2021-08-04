4b6084051c8839d052a4e864a6e8e89a5f080879
mateusoro <mateusoro@gmail.com>
2021-08-04 10:18:13 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -625,7 +625,8 @@ async function iniciar(nome_acao, solicitacao) {
 
                                                         if ((estrategia.Resultado) / (estrategia.Dias) > maior &&
                                                             estrategia.Dias > 5 &&
-                                                            estrategia.Operacoes.length > quant_operacoes && estrategia.quant_stops < quant_stop && estrategia.quant_perdas < quant_perdas && estrategia.quant_vitorias > quant_acertos) {
+                                                            estrategia.Operacoes.length > quant_operacoes && 
+                                                            estrategia.quant_stops < quant_stop && estrategia.quant_perdas < quant_perdas && estrategia.quant_vitorias > quant_acertos) {
 
                                                             maior = (estrategia.Resultado) / (estrategia.Dias);
                                                             estrategia.id = id;
