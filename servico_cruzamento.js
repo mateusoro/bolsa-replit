e500b565214ed2d7a4575ad0851d77fa527cf91f
mateusoro <mateusoro@gmail.com>
2021-08-04 09:28:46 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -568,7 +568,7 @@ async function iniciar(nome_acao, solicitacao) {
         var docs = await sqlite.all('select * from');
         var parar = false;
         if(docs){
-            if(docs[0].ca)
+            if(docs[0].campo=="")
         }
         //console.log(docs);
         for (var x in docs) {
