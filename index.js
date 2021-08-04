b08536ac4dba0762eda11952d574d9e217498f48
mateusoro <mateusoro@gmail.com>
2021-08-04 10:42:29 -0300

    Added script

--- index.js
+++ index.js
@@ -118,6 +118,7 @@ io.sockets.on('connection', (socket) => {
         
         for (var x in docs) {
             var d = JSON.parse(docs[x].campo);
+            io.emit('predefinidos', docs[x]);
         }
         var docs = await db.predefinido.find({});
 
