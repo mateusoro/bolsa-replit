361b67aa8706ff5ebf1d6ad7bcb5e37045e35c71
mateusoro <mateusoro@gmail.com>
2021-08-04 10:40:49 -0300

    Added script

--- teste_banco.js
+++ teste_banco.js
@@ -9,7 +9,7 @@ db.predefinido = new Datastore({ filename: 'requisicoes/predefinodo.json', autol
   var docs = await db.predefinido.find({});
 
   for (var x in docs) {
-    await sqlite.run("insert into requisicao values (null, '"+JSON.stringify(msg)+"', 'S')")
+    await sqlite.run("insert into requisicao values (null, '"+JSON.stringify(docs[x])+"', 'S')")
     io.emit('predefinidos', docs[x]);
   }
 
