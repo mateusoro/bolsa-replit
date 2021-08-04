7df9ed15ceae30880267f6188ae6b7ad9b0ea3d1
mateusoro <mateusoro@gmail.com>
2021-08-04 10:39:42 -0300

    Added script

--- teste_banco.js
+++ teste_banco.js
@@ -1,7 +1,7 @@
 const sqlite = require("aa-sqlite");
 var Datastore = require('nedb-promise');
 var db = {};
-
+db.predefinido = new Datastore({ filename: 'requisicoes/predefinodo.json', autoload: true });
 (async () => {
   console.log(await sqlite.open('sqlite.db'))
   var docs = await sqlite.all('select * from status')
