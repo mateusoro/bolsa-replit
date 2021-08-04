e1ee7bba4b42b068f467585de4f06269e44a8e9c
mateusoro <mateusoro@gmail.com>
2021-08-04 09:21:29 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -13,7 +13,7 @@ const sqlite = require("aa-sqlite");
   })()
 
 var db = {};
-db.requisicao = new Datastore({ filename: 'requisicoes/requisicao.json', autoload: true });
+db.requisicao = new Datastore({ filename: 'requisicoes/requisicao2.json', autoload: true });
 db.retorno = new Datastore({ filename: 'requisicoes/retorno2.json', autoload: true });
 db.grafico = new Datastore({ filename: 'requisicoes/grafico.json', autoload: true });
 db.predefinido = new Datastore({ filename: 'requisicoes/predefinodo2.json', autoload: true });
