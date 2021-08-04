40a779224a8dc2f7b041e4417e2c9a53b75a47e2
mateusoro <mateusoro@gmail.com>
2021-08-04 10:51:18 -0300

    Added script

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ io.sockets.on('connection', (socket) => {
 
         console.log('Salvando');
         //iniciar_cruzamente(msg);
-        await sqlite.run("insert into grafico values (null, '"+JSON.stringify(rr)+"', 'S')")    
+        await sqlite.run("insert into grafico values (null, '"+JSON.stringify(msg)+"', 'S')")    
         await db.predefinido.insert(msg);
 
     });
