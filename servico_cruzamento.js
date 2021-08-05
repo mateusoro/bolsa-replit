343f7465dbd5398599da1fe3dd86997d441ff105
mateusoro <mateusoro@gmail.com>
2021-08-05 11:01:54 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -450,7 +450,7 @@ function crosser(acao, estrategias, stop, tipo) {
                             est.sinal[a] = '';
                         }
                         if (est.indicador == 'aroonosc') {
-                           console.log('Vender aroon')
+                           console.log('Vender aroonosc')
                         }
                     }
                 }
