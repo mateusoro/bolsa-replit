2e440be2fddee9933e6ae84b1879885324bafea7
mateusoro <mateusoro@gmail.com>
2021-08-05 15:46:35 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -131,6 +131,12 @@ async function adx(acao, tamanho) {
     result = corrige_tamanho(t, result[0]);
     return result;
 }
+async function adx(acao, tamanho) {
+    var result = await tulind.indicators.adx.indicator([acao.high, acao.low, acao.close], [tamanho]);
+    var t = acao.close.length;
+    result = corrige_tamanho(t, result[0]);
+    return result;
+}
 .di(high, low, close, 5)
 async function aroonosc(acao, tamanho) {
     var result = await tulind.indicators.aroonosc.indicator([acao.high, acao.low], [tamanho]);
