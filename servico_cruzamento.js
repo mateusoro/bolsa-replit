34fcab927543542f4c3eb998e3879291e5ac41b7
mateusoro <mateusoro@gmail.com>
2021-08-05 15:49:25 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -221,7 +221,7 @@ async function escolher_indicador(acao, indicador, v) {
         resultado = await stochrsi(acao, v);
     }
     if (indicador == 'di_minus') {
-        resultado = await stochrsi(acao, v);
+        resultado = await di(acao, v);
     }
     return resultado;
 
