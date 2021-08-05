de9403e03fc21d762b81c1ab5d79ceb160eabf5c
mateusoro <mateusoro@gmail.com>
2021-08-05 15:49:39 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -223,7 +223,7 @@ async function escolher_indicador(acao, indicador, v) {
     if (indicador == 'di_minus') {
         resultado = await di_minus(acao, v);
     }
-    if (indicador == 'di_l') {
+    if (indicador == 'di_plus') {
         resultado = await di_minus(acao, v);
     }
     return resultado;
