3cf20b5e8bd55021ef817698f6ae77fa87697b20
mateusoro <mateusoro@gmail.com>
2021-08-06 11:09:13 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -505,8 +505,7 @@ function crosser(acao, estrategias, stop, tipo) {
                     sinal_compra = "Comprar";
                     sinal_venda = "Comprar";
                 }
-                if (quant_neutro_venda >= quant_venda) {
-                    sinal_compra = "";
+                if (quant_neutro_venda >= quant_venda) {                   
                     sinal_venda = "";
                 }
                 if (quant_neutro_venda >= quant_venda && quant_neutro >= quant_compra) {
