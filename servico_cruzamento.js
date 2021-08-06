27b4082774ab6fb482c6595e1b86b887de18255d
mateusoro <mateusoro@gmail.com>
2021-08-06 11:38:48 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -312,7 +312,7 @@ function sinal(lista_curta, parametro_fixo, lista_longa, operador) {
         }
 
     }
-    return [lista_retorno, lista_tendencia];
+    return [lista_retorno, lista_tendencia,lista_cruzamento];
 
 
 }
