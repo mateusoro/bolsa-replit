af117f3f0da1e6507587bf527d028e42a751760b
mateusoro <mateusoro@gmail.com>
2021-08-06 11:30:45 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -277,7 +277,7 @@ function sinal(lista_curta, parametro_fixo, lista_longa, operador) {
             novo_parametro_anterior = lista_longa[a - 1];
         }
 
-        var tendencia = ''
+        var tendencia = '';
         if (lista_curta[a - 1] && lista_curta[a - 2]) {
 
             if (lista_curta[a] > lista_curta[a - 1] && lista_curta[a - 1] > lista_curta[a - 2]) { lista_tendencia.push("Cima") }
