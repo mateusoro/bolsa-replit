8fe92554ea6810da2443a56d701c277051b0bebb
mateusoro <mateusoro@gmail.com>
2021-08-06 11:57:32 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -404,7 +404,7 @@ function crosser(acao, estrategias, stop) {
 
                 if (est.tipo == 'Venda') {
                     if (est.indicador == 'stochrsi') {
-                        if (est.tendencia[a] != 'Baixo') {                           
+                        if (est.[a] != 'Baixo') {                           
 
                             est.sinal[a] = '';
                         } else { 'vende stochrsi' }
