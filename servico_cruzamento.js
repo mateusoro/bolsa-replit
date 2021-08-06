520d30d82e35b2e690190cc0c8f5ef72acd5a05e
mateusoro <mateusoro@gmail.com>
2021-08-06 11:19:41 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -672,7 +672,7 @@ async function iniciar(nome_acao, solicitacao) {
                                                                                             if (longa_maior10) {
 
                                                                                                 //console.log('entrou 1')
-                                                                                                const estrategia = crosser(petr4, estrategias, sto, solicitacao.tipo_cruzamento);
+                                                                                                const estrategia = crosser(petr4, estrategias, sto, '');
                                                                                                 //console.log((estrategia.Resultado) / (estrategia.Dias) > maior, estrategia.Dias > 5, estrategia.Operacoes.length > quant_operacoes, estrategia.quant_stops < quant_stop, estrategia.quant_perdas < quant_perdas, estrategia.quant_vitorias > quant_acertos, estrategia.quant_perdas, quant_perdas, estrategia.quant_vitorias, quant_acertos);
 
                                                                                                 if ((estrategia.Resultado) / (estrategia.Dias) > maior &&
