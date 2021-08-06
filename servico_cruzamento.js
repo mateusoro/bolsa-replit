6c45768fd5a63e39830950c4029d399ace3a0243
mateusoro <mateusoro@gmail.com>
2021-08-06 11:34:32 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -277,7 +277,7 @@ function sinal(lista_curta, parametro_fixo, lista_longa, operador) {
             novo_parametro_anterior = lista_longa[a - 1];
         }
         if (lista_longa[a - 2]) {
-            novo_parametro_anterior2 = lista_longa[a - 1];
+            novo_parametro_anterior2 = lista_longa[a - 2];
         }
 
         var tendencia = '';
