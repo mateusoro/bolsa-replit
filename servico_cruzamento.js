179bd8001730c0cc6dc218d950560e87152311e2
mateusoro <mateusoro@gmail.com>
2021-08-06 11:31:55 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -289,7 +289,7 @@ function sinal(lista_curta, parametro_fixo, lista_longa, operador) {
         } else {
             tendencia = "Lateralizado";
         }
-        lista_tendencia.pi
+        lista_tendencia.push(d)
         if (operador == 'normal') {
             if (lista_curta[a] > novo_parametro) { lista_retorno.push("Comprar") }
             if (lista_curta[a] < novo_parametro) { lista_retorno.push("Vender") }
