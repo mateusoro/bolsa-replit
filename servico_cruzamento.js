b18366552c08f92eca88776b64f9511cca0964e1
mateusoro <mateusoro@gmail.com>
2021-08-06 09:31:09 -0300

    Added script

--- servico_cruzamento.js
+++ servico_cruzamento.js
@@ -194,7 +194,9 @@ async function escolher_estrategia_tipo(acao, estra, index, v1, v2, longa_maior_
                 estra[index].indicador_valores_adx = await escolher_indicador(acao, 'adx', v2);
                 estra[index].sinal_temp = sinal(estra[index].indicador_valores_compra, 0, estra[index].indicador_valores_venda, estra[index].operador)[0];
                 estra[index].sinal_adx = sinal(estra[index].indicador_valores_adx, v2, null, 'normal')[0];
-                for(var temp in  estra[index].sinal_temp )
+                for(var temp in  estra[index].sinal_temp ){
+                    
+                }
             
             
             }else{
